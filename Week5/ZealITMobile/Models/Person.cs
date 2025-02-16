7f65e5d501bd58abacfc87eabc20e4e2b8c3604f
Homero Lara <homero.lara@gmail.com>
2025-02-16 14:40:12 -0600

    WIP: Added .Net Interactive Notebook examples.

---
+++ Week5/ZealITMobile/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace ZealITMobile.Models;
+
+public class Person
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
